b042518dbe8d808e270d4c7075376b0853182c57
MOUGOUE KAKANOU ROLLY DAVANY <99815109+Rolly-M@users.noreply.github.com>
2024-06-27 20:10:53 +0300

    Docker install script reworked 1.1

--- installdocker.sh
+++ installdocker.sh
@@ -23,3 +23,5 @@ sudo apt-get update
 
 #Install Docker Engine, containerd, and Docker Compose.
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
+exit
