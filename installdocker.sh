11fd104695e691f9a95bd1913477ef88009c7572
MOUGOUE KAKANOU ROLLY DAVANY <99815109+Rolly-M@users.noreply.github.com>
2024-06-27 20:03:07 +0300

    Docker install script reworked

--- installdocker.sh
+++ installdocker.sh
@@ -3,16 +3,16 @@
 # Make sure to run a sudo chmod +x installdocker.sh to make this script executable
 
 #uninstall existing docker installation
-sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt-get autoremove --purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
 #Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg lsb-release || true
     
 #Add Docker’s official GPG key:    
-sudo mkdir -m 0755 -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 #Use the following command to set up the repository:
