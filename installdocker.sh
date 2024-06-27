a73363376729f63cf697d7d52a634c7ca6bb0ffb
MOUGOUE KAKANOU ROLLY DAVANY <99815109+Rolly-M@users.noreply.github.com>
2024-06-27 19:51:50 +0300

    ...

--- installdocker.sh
+++ installdocker.sh
@@ -16,10 +16,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 #Use the following command to set up the repository:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || true
 sudo apt-get update
 
 #Install Docker Engine, containerd, and Docker Compose.
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose || true
-
-Exit
