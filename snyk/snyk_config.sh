afece8d291f523c310ce0c8eecfdfbfde6fbe2cb
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2024-07-14 00:23:23 +0300

    Cleanup 1

--- snyk/snyk_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl https://static.snyk.io/cli/latest/snyk-linux -o snyk
-chmod +x ./snyk
-mv ./snyk /usr/local/bin/ 
-
-SNYK_TOKEN=$SNYK_API_TOKEN
-
-snyk test --all-projects --org=78b2f53f-b8ed-4a42-940f-60f8f8f18274
